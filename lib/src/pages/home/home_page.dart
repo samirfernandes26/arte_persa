c8739a33cc04469eab8c9596ee3352997d61d07b
Samir Fernandes de Lima Resende <samirfernandes@gmail.com>
2024-02-21 11:30:11 -0300

    Iniciando home

--- lib/src/pages/home/home_page.dart
+++ lib/src/pages/home/home_page.dart
@@ -1,3 +1,55 @@
-abstract class HomePage {
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+
+class HomePage extends ConsumerStatefulWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  ConsumerState<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends ConsumerState<HomePage> {
+  final formKey = GlobalKey<FormBuilderState>();
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
   
+  @override
+  Widget build(BuildContext context) {
+    throw Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {},
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(32.0),
+          child: FormBuilder(
+            key: formKey,
+            child: const Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                // Lottie.asset(
+                //   'assets/lotties/register_arte_persa.json',
+                //   height: 160,
+                //   fit: BoxFit.fill,
+                // ),
+                Text('data')
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
 }
