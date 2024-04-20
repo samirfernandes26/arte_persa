824007c0870e91bef650001dd9a36f95d786bf91
Samir Fernandes de Lima Resende <samirfernandes26@gmail.com>
2024-04-20 18:08:46 -0300

    Salvar

--- lib/src/pages/cadastro_cliente/cadastro_cliente_endereco_page.dart
+++ lib/src/pages/cadastro_cliente/cadastro_cliente_endereco_page.dart
@@ -262,7 +262,6 @@ class _CadastroClienteEnderecoPageState
                         RouteGeneratorKeys.home,
                       );
                     }
-
                     break;
                 }
               },
