cacc4e7d2afb79689f4f3c32f49d502c0b90f75f
Samir <samirresende26@gmail.com>
2026-05-24 13:00:49 -0300

    chore(codegen): add generated provider setup

--- lib/src/core/providers/provider.dart
+++ lib/src/core/providers/provider.dart
@@ -1,3 +1,10 @@
+import 'package:arte_persa/src/core/rest_client/rest_client.dart';
+import 'package:arte_persa/src/shared/infra/database/database.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart' show Ref;
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'provider.g.dart';
+
 @Riverpod(keepAlive: true)
 RestClient restClient(Ref ref) => RestClient();
 
---
+++ lib/src/core/providers/provider.g.dart
@@ -0,0 +1,42 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$restClientHash() => r'9ac7d91d216dbfa8d4c4a1dfc24e01b38bf3f0a5';
+
+/// See also [restClient].
+@ProviderFor(restClient)
+final restClientProvider = Provider<RestClient>.internal(
+  restClient,
+  name: r'restClientProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$restClientHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef RestClientRef = ProviderRef<RestClient>;
+String _$databaseHash() => r'e5e8032eeec6b863744868c1bdeb45adf83b02a8';
+
+/// See also [database].
+@ProviderFor(database)
+final databaseProvider = Provider<Database>.internal(
+  database,
+  name: r'databaseProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$databaseHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef DatabaseRef = ProviderRef<Database>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
